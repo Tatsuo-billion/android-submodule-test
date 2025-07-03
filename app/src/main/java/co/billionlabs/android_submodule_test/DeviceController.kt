16648fb420d253969face7c9511b6bd4342571a8
Tatsuo-billion <tatsuo@billionlabs.co>
2025-07-03 16:47:55 -0700

    initial commit of android project after gitignore update

---
+++ app/src/main/java/co/billionlabs/android_submodule_test/DeviceController.kt
@@ -0,0 +1,19 @@
+package co.billionlabs.android_submodule_test
+
+class DeviceController {
+
+    private var parameterSize = 7
+    private var parameters: DoubleArray = DoubleArray(parameterSize)
+
+    var registeredDeviceSet: Collection<String> =
+        setOf(
+            "Pixel 4",
+            "Pixel 8",
+            "SM-A536U1",
+            "SM-S921U1",
+            "SM-A145M",
+            "SM-S931U1",
+            "moto g power (2022)",
+            "moto g 5G - 2024",
+        )
+}
